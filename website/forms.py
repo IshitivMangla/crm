842e114f2958ce194a966f447b90ac00aefa5267
IshitivMangla <ishitivmangla@outlook.com>
2025-04-12 18:58:28 +0530

    user form created

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Email Address'}))
@@ -9,7 +10,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = {'username' , 'first_name', 'last_name', 'email', 'password1', 'password2'}
+        fields = ['first_name', 'last_name','email', 'username' , 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
@@ -44,3 +45,8 @@ class SignUpForm(UserCreationForm):
             '</small></span>'
         )
 	
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already registered.")
+        return email
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -28,4 +29,20 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Registered Successfully")
+                return redirect('home')
+            else:
+                messages.error(request, "Authentication failed")
+    else:
+        form = SignUpForm()
+        
+    return render(request, 'register.html', {'form': form})
